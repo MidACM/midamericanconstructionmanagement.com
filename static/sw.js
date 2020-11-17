dea25a3f2b0aa5fe5d5e60141c33950ff2e0a2e1
Cloud Radix <kbutton@cloudradix.com>
2020-11-16 23:31:09 -0500

    update snippets for forestry

--- static/sw.js
+++ static/sw.js
@@ -1,19 +1,3 @@
-/*
-Copyright 2018 Google Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-// TODO - update when Workbox v3 is published
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
 
 workbox.precaching.precacheAndRoute([]);
@@ -24,7 +8,7 @@ self.addEventListener('install', event => {
     'https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.js',
     'https://cdn.ampproject.org/shadow-v0.js',
     'index.html',
-    '/'
+    '/*'
   ];
   const cacheName = workbox.core.cacheNames.runtime;
   event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(urls)));
